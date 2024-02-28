70b4c66c6d4a863bff8642ba2cd4e11c98ee0794
João Paulo <joaopaulomayka@gmail.com>
2024-02-28 17:51:23 -0300

    feat: search for the best rated films
    creation of the getTopRated method to make a request to the TMDB API and search for the best rated films

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+
+const moviesURL = import.meta.env.VITE_API;
+const apikey = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
+
+  const[topMovies, setTopmMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopmMovies(data.results)
+  }
+  useEffect(() =>{
+    const topRatedUrl = `${moviesURL}top_rated?${apikey}`;
+    getTopRatedMovies(topRatedUrl)
+
+
+
+  },[])
+  
   return (
-    <div>Home</div>
+    <div className="container">
+      <h2 className="title">Melhores Avaliações:</h2>
+      <div className="movies-container">
+        {topMovies.length > 0 && topMovies.map((movie) => <p>{movie.title}</p> )}
+      </div>
+    </div>
   )
 }
 
